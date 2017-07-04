91de49784d4205810ab4da4a79fbd3ed56ab909b
Heath Alexander <ambientsidewalk@gmail.com>
2017-07-03 21:37:57 -0500

    First working prototype

--- create_docker_worker.py
+++ create_docker_worker.py
@@ -1,6 +1,6 @@
 import sys, getopt, boto3
 
-client = boto3.client('ec2')
+
 # def main(argv):
 #     release = 'prod'
 #     try:
@@ -17,7 +17,7 @@ client = boto3.client('ec2')
 #         elif opt in ("-o", "--ofile"):
 #             outputfile = arg
 
-
+EC2_CLIENT = ec2_client=boto3.client('ec2')
 DEFAULT_AMI_TAG_TYPE = 'Docker Base'
 DEFAULT_AMI_TAG_RELEASE = 'Prod'
 RELEASE_TAG_VALUES = ['Prod', 'UAT', 'Test']
@@ -37,10 +37,7 @@ def get_docker_ami(**kwargs):
     release_image_tag -- default 'Prod'
     """
 
-    if 'ec2_client' not in kwargs:
-        raise Exception('Missing required parameter ec2_client')
-
-    ec2_client = kwargs.get('ec2_client')
+    ec2_client = kwargs.get('ec2_client', EC2_CLIENT)
     image_type = kwargs.get('type_image_tag', DEFAULT_AMI_TAG_TYPE)
     release = kwargs.get('release_image_tag', DEFAULT_AMI_TAG_RELEASE)
 
@@ -75,8 +72,29 @@ def get_docker_ami(**kwargs):
 NETWORK_SEGMENT_TAG_VALUES = ('MW', 'DMZ', 'PUB', 'INT', 'DB', 'ADMIN')
 
 
+def get_best_subnet_id(**kwargs):
+    ec2_client = kwargs.get('ec2_client', EC2_CLIENT)
+    environment_name = kwargs.get('environment_name')
+    network_segment = kwargs.get('network_segment')
+
+    subnets = get_subnet_ids(
+        ec2_client=ec2_client,
+        environment_name=environment_name,
+        network_segment=network_segment
+    )
+
+    available_ips=0
+    subnet_id=''
+    for subnet in subnets:
+        if subnet.get('AvailableIpAddressCount') > available_ips:
+            available_ips=subnet.get('AvailableIpAddressCount')
+            subnet_id=subnet.get('SubnetId')
+
+    return subnet_id
+
+
 def get_subnet_ids(**kwargs):
-    """get the subnet id of  with 'Environment_Name:environment_name_subnet_tag' and
+    """get the subnets with 'Environment_Name:environment_name_subnet_tag' and
     'Network_Segnment:network_segment_subnet_tag'  where release is one of the following values:
         (MW,DMZ,PUB,INT,DB,ADMIN)
 
@@ -85,20 +103,24 @@ def get_subnet_ids(**kwargs):
 
     Keyword arguments:
     ec2_client (required) -- an amazon BOTO3 EC2 client
-    environment_name_subnet_tag (required) -- environment name
-    network_segment_subnet_tag -- default 'MW'
+    environment_name (required) -- environment name
+    network_segment -- default 'MW'
     availability_zone -- if specified filter to this AZ only
+
+    Returns:
+        List of
+         {'AvailabilityZone': str,
+           'AvailableIpAddressCount': int,
+           'SubnetId': str
+         }
     """
 
-    if 'ec2_client' not in kwargs:
-        raise Exception('Missing required parameter ec2_client')
+    ec2_client = kwargs.get('ec2_client', EC2_CLIENT)
+    if 'environment_name' not in kwargs:
+        raise Exception('Missing required parameter environment_name')
+    environment_name = kwargs.get('environment_name')
 
-    if 'environment_name_subnet_tag' not in kwargs:
-        raise Exception('Missing required parameter environment_name_subnet_tag')
-
-    ec2_client = kwargs.get('ec2_client')
-    environment_name = kwargs.get('environment_name_subnet_tag')
-    network_segment = kwargs.get('network_segment_subnet_tag', 'MW')
+    network_segment = kwargs.get('network_segment', 'MW')
     az = kwargs.get('availability_zone', '')
 
     if network_segment not in NETWORK_SEGMENT_TAG_VALUES:
@@ -146,17 +168,20 @@ def get_next_available_name(**kwargs):
 
     Keyword arguments:
     ec2_client (required) -- an amazon BOTO3 EC2 client
-    base_instance_name (required) -- environment name
+    environment_name (required) -- environment name
+    network_segment (required) -- network_segment
     """
 
-    if 'ec2_client' not in kwargs:
-        raise Exception('Missing required parameter ec2_client')
+    ec2_client = kwargs.get('ec2_client', EC2_CLIENT)
+    if 'environment_name' not in kwargs:
+        raise Exception('Missing required parameter environment_name')
+    env_name = kwargs.get('environment_name')
 
-    if 'base_instance_name' not in kwargs:
+    if 'network_segment' not in kwargs:
         raise Exception('Missing required parameter base_instance_name')
+    network_segment = kwargs.get('network_segment')
 
-    ec2_client = kwargs.get('ec2_client')
-    base_name = kwargs.get('base_instance_name')
+    base_name = env_name.lower()+'-'+network_segment.lower()+'-docker'
 
     response = ec2_client.describe_instances(
         Filters=[
@@ -173,50 +198,59 @@ def get_next_available_name(**kwargs):
             names += [tag['Value'] for tag in instance['Tags'] if tag['Key'] == 'Name']
 
     count = 1
-    while base_name+'0a'+format(count, '02d') in names:
+    while base_name+format(count, '02d') in names:
         count += 1
-    new_name = base_name+'0a'+format(count, '02d')
+    new_name = base_name+format(count, '02d')
 
     return new_name
 
 
-def create_instance(**kwargs):
-    if 'ec2_client' not in kwargs:
-        raise Exception('Missing required parameter ec2_client')
-
-    if 'ami_id' not in kwargs:
-        raise Exception('Missing required parameter ami_id')
-
-    if 'env_name' not in kwargs:
-        raise Exception('Missing required parameter env_name')
+def create_docker_instance(**kwargs):
+    ec2_client = kwargs.get('ec2_client', EC2_CLIENT)
+    if 'environment_name' not in kwargs:
+        raise Exception('Missing required parameter environment_name')
+    env_name = kwargs.get('environment_name')
 
     if 'network_segment' not in kwargs:
         raise Exception('Missing required parameter network_segment')
-
-    ec2_client = kwargs.get('ec2_client')
-    ami_id = kwargs.get('ami_id')
-    instance_type = kwargs.get('instance_type', 't2.medium')
-    env_name = kwargs.get('env_name')
     network_segment = kwargs.get('network_segment')
+
+    release = kwargs.get('release', 'PROD')
+    key_name = kwargs.get('key_name', 'stratos-rootish')
+    security_group_ids = [
+        'sg-fc846784',  #default
+        'sg-bd21d7c6',  #phonehome
+        'sg-650d831d'   #nat
+    ]
+    instance_type = kwargs.get('instance_type', 't2.medium')
     project = kwargs.get('project', 'Unassigned')
     team = kwargs.get('team', 'Unassigned')
 
+    ami_id = get_docker_ami(
+        ec2_client=boto3.client('ec2'),
+        release=release)
 
     instance_name = get_next_available_name(
         ec2_client=ec2_client,
-        base_instance_name=env_name+'-'+network_segment+'-docker'
-    )
+        environment_name=env_name,
+        network_segment=network_segment)
 
+    subnet_id = get_best_subnet_id(
+        ec2_client=ec2_client,
+        environment_name=env_name,
+        network_segment=network_segment
+    )
     response = ec2_client.run_instances(
         ImageId=ami_id,
         InstanceType=instance_type,
-        SecurityGroups=[
-            'string',
-        ],
+        MinCount=1,
+        MaxCount=1,
+        KeyName=key_name,
+        SecurityGroupIds=security_group_ids,
         SubnetId=subnet_id,
         TagSpecifications=[
             {
-                'ResourceType': 'instance''volume',
+                'ResourceType': 'volume',
                 'Tags': [
                     {
                         'Key': 'Environment_Name',
@@ -237,7 +271,7 @@ def create_instance(**kwargs):
                 ]
             },
             {
-                'ResourceType': 'instance''volume',
+                'ResourceType': 'instance',
                 'Tags': [
                     {
                         'Key': 'Environment_Name',
@@ -259,3 +293,7 @@ def create_instance(**kwargs):
             }
         ]
     )
+    result = {
+        'imageId': response['Instances'][0]['imageId'],
+        'ip': response['Instances'][0]['PublicIpAddress']}
+    return result
--- test_get_subnet_id.py
+++ test_get_subnet_id.py
@@ -6,15 +6,34 @@ class TestGet_docker_ami(TestCase):
 
     def test_get_docker_ami(self):
         get_subnet_ids(ec2_client=boto3.client('ec2'),
-                       environment_name_subnet_tag='PRQA1',
-                       network_segment_subnet_tag='MW',
+                       environment_name='PRQA1',
+                       network_segment='MW',
                        availability_zone='us-east-1e')
 
     def test_get_next_available_name(self):
-        get_next_available_name(ec2_client=boto3.client('ec2'),
-                                base_instance_name='prqa1-dmz-docker')
+        name = get_next_available_name(ec2_client=boto3.client('ec2'),
+                                       environment_name='PRQA1',
+                                       network_segment='DMZ')
+        self.assertEqual(name, 'prqa1-dmz-docker05')
+
+    def test_get_best_subnet_id(self):
+        subnet_id = get_best_subnet_id(
+            ec2_client=ec2_client,
+            environment_name='PRQA1',
+            network_segment='DMZ'
+        )
+        self.assertEqual(subnet_id, '')
 
     def test_get_docker_ami(self):
-        ami_id = get_docker_ami(ec2_client=boto3.client('ec2'), release='Prod')
-        self.assertEquals(ami_id,'')
+        ami_id = get_docker_ami(ec2_client=boto3.client('ec2'),
+                                release='Prod')
+        self.assertEqual(ami_id, 'ami-3483be22')
+
+    def test_create_docker_instance(self):
+        result = create_docker_instance(
+            ec2_client=boto3.client('ec2'),
+            environment_name='PRQA1',
+            network_segment='DMZ',
+            project='Prospect',
+            team='Twix')
 
